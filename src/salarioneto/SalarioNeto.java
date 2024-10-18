894ef9040a9c8b26a4fd61a53e007e07c96a5dd9
alceronDelgado <luis_aceron@soy.sena.edu.co>
2024-10-18 14:59:19 -0500

    Agregado ejercicio

--- src/salarioneto/SalarioNeto.java
+++ src/salarioneto/SalarioNeto.java
@@ -15,8 +15,30 @@ public class SalarioNeto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "hello world");
-        JOptionPane.showMessageDialog(null, "hello world2");
-    }
+        int cedula,vinculacion,porcentaje,descuentoSalario,salarioBasico,salarioNeto;
+        String mensaje;
+        
+        //Guarda la cedula, usamos el parse int porque todo elemento de un JOptionPane que reciba lo recibe en formato de string, hay que transformarlo a Entero.
+        cedula = Integer.parseInt(JOptionPane.showInputDialog("digite su cedula por favor"));
+        salarioBasico = Integer.parseInt(JOptionPane.showInputDialog("Digite el salario básico del empleado"));
+        vinculacion = Integer.parseInt(JOptionPane.showInputDialog("Digite el año de su vinculación a la empresa"));
+        
+        if((salarioBasico > 1200000) && (vinculacion > 1990)){
+            //Se descuenta un 8% de su salario
+            mensaje = "Se descuenta un 8% de tu salario";
+            porcentaje = 8;
+        }else if((salarioBasico < 550000) || (vinculacion == 1990)){
+            mensaje = "Se descuenta un 2% de tu salario";
+            porcentaje = 2;
+        }else{
+            mensaje = "Se descuenta un 5% de descuento";
+            porcentaje = 5;
+        }
+        
+        descuentoSalario = (salarioBasico * porcentaje) / 100;
+        salarioNeto = salarioBasico - descuentoSalario;
+        
+        JOptionPane.showMessageDialog(null, " "+mensaje+" \n Nro cedula: "+cedula+"\n Salario Neto: "+salarioNeto);
     
+    }
 }
