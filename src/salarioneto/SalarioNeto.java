4d74bb1af4c429780faaf59b4ef885023ae66190
lAlejandrocd <lalejandrocd1@gmail.com>
2024-10-18 14:30:29 -0500

    Add JOptionPane

--- src/salarioneto/SalarioNeto.java
+++ src/salarioneto/SalarioNeto.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package salarioneto;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,7 +15,7 @@ public class SalarioNeto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "hello world");
     }
     
 }
